8d9973ae77f9df6e5d7ab98622ce03feefde8ec1
Neimhin Robinson Gunning <nrobinso@tcd.ie>
2020-08-17 13:33:09 +0100

    basic c++ practice

---
+++ c++_practice/practice_1/basics.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int square(int base){
+	return base*base;
+}
+
+int main(int argc, char** argv){
+	std::cout << "Hello World!";
+	
+	std:: cout << std::endl << "base = 2, power = 2" << std::endl;
+
+	std::cout << square(2);
+}
