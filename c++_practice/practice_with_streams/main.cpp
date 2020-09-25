3720ef07cf15f2889b668ddddfe8b6606fdaefc9
Neimhin Robinson Gunning <nrobinso@tcd.ie>
2020-09-25 16:20:08 +0100

    learning streams

---
+++ c++_practice/practice_with_streams/main.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[]){
+    std::ofstream outfile;
+    outfile.open("file created by ofstream", std::ios::out | std::ios::trunc);
+
+    outfile << "Hello new file!\n";
+    outfile.close();
+    return 0;
+}
